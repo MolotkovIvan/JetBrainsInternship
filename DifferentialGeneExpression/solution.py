a1b8112240be822546ecabcc0ddb0aa36521c4c2
MolotkovIvan <31643933+MolotkovIvan@users.noreply.github.com>
2020-04-05 19:32:32 +0300

    Update solution.py

--- DifferentialGeneExpression/solution.py
+++ DifferentialGeneExpression/solution.py
@@ -1,3 +1,17 @@
+def print_differing(ratio):
+	for k in dictlist[0]:
+		v1 = dictlist[0][k]
+		v2 = dictlist[1][k]
+		v3 = dictlist[2][k] 
+		if (v1/v2 > ratio or v1/v3 > ratio or v2/v1 > ratio or v2/v3 > ratio or v3/v1 > ratio or v3/v2 > ratio):
+			print(k, ":\t", dictlist[0][k], dictlist[1][k], dictlist[2][k])
+
+
+def print_avg():
+	for k in dictlist[0]:
+		print(k, "\t", dictlist[0][k], dictlist[1][k], dictlist[2][k])
+
+
 dictlist = [dict() for x in range (3)]
 avg = [0,0,0]
 
@@ -10,13 +24,4 @@ for i in range(3):
 		dictlist[i][name] = dictlist[i].get(name, 0) + float(value)/6
 	avg[i] /= len(dictlist[i])
 
-def print_differing(ratio):
-	for k in dictlist[0]:
-		v1 = dictlist[0][k]
-		v2 = dictlist[1][k]
-		v3 = dictlist[2][k] 
-		if (v1/v2 > ratio or v1/v3 > ratio or v2/v1 > ratio or v2/v3 > ratio or v3/v1 > ratio or v3/v2 > ratio):
-			print(k, "\t", dictlist[0][k], dictlist[1][k], dictlist[2][k])
-
-def print_avg():
-	print(avg[0], avg[1], avg[2])
+print_differing(1.45)
